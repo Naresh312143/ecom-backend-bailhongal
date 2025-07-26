685f360cb090349803109b6446dfc356c6239d2b
Naresh312143 <snaresh3102@gmail.com>
2025-07-26 15:49:40 +0530

    Cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -102,6 +102,40 @@ const getCart = async (req, res, next) => {
   }
 };
 
+// clear cart
+const clearCart = async (req, res, next) => {
+  try {
+    // Get userId from request
+    const userId = req.user._id;
+
+    // Find the cart for the user
+    const cart = await Cart.findOne({ userId });
+
+    // If cart does not exist, return a message
+    if (!cart) {
+      return res.status(404).json({
+        message: "Cart not found",
+      });
+    }
+
+    // Clear the cart
+    cart.products = [];
+    cart.totalPrice = 0;
+    await cart.save();
+
+    // Return success message
+    res.status(200).json({
+      success: true,
+      message: "Cart cleared successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
+
 // Helper to calculate total price
 async function calculateTotalPrice(products) {
   let total = 0;
